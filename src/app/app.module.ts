46969cdb88b489f070dec3b752a7ba77b7003081
GreenM4x <greenmax02@gmail.com>
2023-08-14 22:11:34 +0200

    start board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BoardComponent } from './containers/board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
